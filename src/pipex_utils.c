22ef68311d1d8fded33f91e46cc2b71d5f36f2e2
mehdisalim <mehdisalimofficial2000@gmail.com>
2023-01-21 11:42:59 +0100

    solve error 2 , 5 and 6 in the second tester (error handling)

--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: esalim <esalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 19:30:32 by esalim            #+#    #+#             */
-/*   Updated: 2023/01/20 19:15:27 by esalim           ###   ########.fr       */
+/*   Updated: 2023/01/21 11:38:14 by esalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ char	**get_env_path(char **env)
 	char	*path;
 
 	i = -1;
-    if (!env)
-		return (ft_split("/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin", ':'));
+    if (!env || !*env)
+		return (ft_split("/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:./", ':'));
 	while (env[++i])
 	{
 		path = ft_strnstr(env[i], "PATH=", 5);
@@ -32,31 +32,7 @@ char	**get_env_path(char **env)
 		exit(1);
 	return (paths);
 }
-/*
-char	**sep_cmd(char *s)
-{
-	char	**cmds;
-	int		i;
-	int		len;
 
-	i = -1;
-	len = ft_strlen(s);
-	cmds = malloc(3 * sizeof(char *));
-	if (!cmds)
-		exit(0);
-	while (s[++i])
-		if (s[i] == ' ' || s[i] == '\t')
-			break ;
-	cmds[0] = ft_substr(s, 0, i);
-	if (i != len)
-		cmds[1] = ft_substr(s, i + 1, len);
-	else
-		cmds[1] = 0;
-	if (cmds[1] != 0)
-		cmds[2] = 0;
-	return (cmds);
-}
-*/
 char    *remove_end_space(char *cmd)
 {
     int i = 0;
@@ -117,24 +93,6 @@ char	**split_commands(char *cmds, char **env)
 {
 	char	**commands;
 	char	*cmd_path;
-
-    /*
-    char *squates = ft_strchr(cmds, 34);
-    char *dquates = ft_strchr(cmds, 39);
-    if (squates && squates[0] == 34 && squates[1] && squates[1] == 39)
-    {
-        ft_printf("awk: syntax error at source line 1\n context is\n	 >>> ' <<<\nawk: bailing out at source line 1\n");
-        exit(2);
-    }
-    else if (dquates && dquates[0] == 39 && dquates[1] && dquates[1] == 34)
-        commands = ft_split(cmds, 39);
-    else if (squates && squates[0] == 34 && squates[1] && squates[1] == 39)
-        commands = ft_split(cmds, 34);
-    else if (ft_strchr(cmds, 39))
-        commands = ft_split(cmds, 39);
-    else if (ft_strchr(cmds, 34) && !ft_strchr(cmds, 39))
-        commands = ft_split(cmds, 34);
-    else*/
     char sep = get_separator(cmds);
     commands = ft_split(cmds, sep);
 	cmd_path = get_cmd_path(commands[0], env);
@@ -143,8 +101,8 @@ char	**split_commands(char *cmds, char **env)
 	    ft_printf("pipex: %s: command not found\n", commands[0]);
         exit(127);
     }
-	free(commands[0]);
+	//free(commands[0]);
 	commands[0] = ft_strdup(cmd_path);
-	free(cmd_path);
+	//free(cmd_path);
 	return (commands);
 }
